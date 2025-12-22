7e1dae5e42b3d6b7a47cc674e09ffefe7d9fc849
uponlydev <dev@uponly.space>
2025-12-22 10:06:53 +0100

    Update pro.rs

--- programs/simple-token/src/pro.rs
+++ programs/simple-token/src/pro.rs
@@ -1235,13 +1235,34 @@ pub struct BuyAndLockToken<'info> {
     #[account(init_if_needed, payer = user, space = 8 + 32 + 8 + 8 + 33 + 1 + 8, seeds = [b"locked", user.key().as_ref()], bump)]
     pub lock_state: Account<'info, LockedTokenState>,
 
-    #[account(mut)]
+    #[account(
+        mut,
+        constraint = user_usdc_account.mint == metadata.payment_token,
+        associated_token::mint = metadata.payment_token,
+        associated_token::authority = user
+    )]
     pub user_usdc_account: Box<Account<'info, TokenAccount>>,
 
-    #[account(mut)]
+    #[account(
+        mut,
+        constraint = deployer_usdc_account.mint == metadata.payment_token,
+        constraint = deployer_usdc_account.key()
+            == anchor_spl::associated_token::get_associated_token_address(
+                &metadata.team,
+                &metadata.payment_token
+            )
+    )]
     pub deployer_usdc_account: Box<Account<'info, TokenAccount>>,
 
-    #[account(mut)]
+    #[account(
+        mut,
+        constraint = program_payment_token_account.mint == metadata.payment_token,
+        constraint = program_payment_token_account.key()
+            == anchor_spl::associated_token::get_associated_token_address(
+                &pool_authority.key(),
+                &metadata.payment_token
+            )
+    )]
     pub program_payment_token_account: Box<Account<'info, TokenAccount>>,
 
     #[account(mut)]
@@ -1292,6 +1313,10 @@ pub struct BuyAndLockToken<'info> {
     #[account(mut)]
     pub founder_pool_token_account: Box<Account<'info, TokenAccount>>,
 
+    #[account(
+        seeds = [b"token_account", metadata.payment_token.as_ref()],
+        bump
+    )]
     /// CHECK: PDA that owns program_payment_token_account
     pub pool_authority: UncheckedAccount<'info>,
 
@@ -1355,7 +1380,12 @@ pub struct LeverageBuy<'info> {
 
     #[account(
         mut,
-        constraint = program_payment_token_account.mint == metadata.payment_token
+        constraint = program_payment_token_account.mint == metadata.payment_token,
+        constraint = program_payment_token_account.key()
+            == anchor_spl::associated_token::get_associated_token_address(
+                &pool_authority.key(),
+                &metadata.payment_token
+            )
     )]
     pub program_payment_token_account: Box<Account<'info, TokenAccount>>,
 
@@ -1473,8 +1503,8 @@ pub struct LeverageSell<'info> {
     pub metadata: Account<'info, TokenMetadata>,
 
     #[account(mut)]
-    ///CHECK: Used to derive vault PDA
-    pub user: UncheckedAccount<'info>,
+    ///CHECK: Used to derive vault PDA; must authorize closing the position
+    pub user: Signer<'info>,
 
     #[account(mut, seeds = [b"leverage", user.key().as_ref()], bump)]
     pub leverage_position: Account<'info, LeveragePosition>,
@@ -1512,7 +1542,12 @@ pub struct LeverageSell<'info> {
 
     #[account(
         mut,
-        constraint = program_payment_token_account.mint == metadata.payment_token
+        constraint = program_payment_token_account.mint == metadata.payment_token,
+        constraint = program_payment_token_account.key()
+            == anchor_spl::associated_token::get_associated_token_address(
+                &pool_authority.key(),
+                &metadata.payment_token
+            )
     )]
     pub program_payment_token_account: Box<Account<'info, TokenAccount>>,
 
@@ -1586,8 +1621,8 @@ pub struct ClaimLockedTokens<'info> {
     pub metadata: Account<'info, TokenMetadata>,
 
     #[account(mut)]
-    ///CHECK: Used to derive vault PDA
-    pub user: UncheckedAccount<'info>,
+    ///CHECK: Used to derive vault PDA; must authorize claim/sell
+    pub user: Signer<'info>,
 
     #[account(
         mut,
